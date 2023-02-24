2b415f5264f2d40195688d73f941a4a2c2324b85
Fixxoo <rajpalmal40@gmail.com>
2023-02-25 01:04:37 +0530

    Added Basic UI

--- src/App.js
+++ src/App.js
@@ -3,31 +3,33 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="text-2xl border-2">
-      <h1 className="text-center">My Todos</h1>
+    <div className="flex justify-center">
+      <div className="text-2xl mx-5 border-2 mt-10 w-5/6">
+        <h1 className="text-center">My Todos</h1>
 
-      <div className="flex border-2">
-        <div className="name flex flex-col ml-5">
-          <label className="text-xl">Name</label>
-          <input className="border-2 w-40 h-7" type={Text} />
+        <div className="flex border-2 px-4 py-2">
+          <div className="name flex flex-col">
+            <label className="text-2xl">Task</label>
+            <input className="border-2 w-40 h-7" type={Text} />
+          </div>
+
+          <div className="description flex flex-col ml-5">
+            <label className="text-2xl">Description</label>
+            <input className="border-2 w-40 h-7" type={Text} />
+          </div>
+
+          <button className="border-2 ml-auto mr-4 w-32">Add Task</button>
         </div>
 
-        <div className="description flex flex-col ml-4">
-          <label className="text-xl">Description</label>
-          <input className="border-2 w-40 h-7" type={Text} />
-        </div>
-
-        <button className="border-2 ml-6">Add</button>
-      </div>
-
-      <div className="flex">
-        <div>
-          <p>Task 1</p>
-          <p>Description</p>
-        </div>
-        <div>
-          <button className="border-2">Tick</button>
-          <button className="border-2">Tick</button>
+        <div className="flex border-2 justify-between items-center mt-5 px-2">
+          <div className="">
+            <p className="text-3xl">Task 1</p>
+            <p>Description</p>
+          </div>
+          <div className="flex justify-between w-52 ">
+            <button className="border-2 w-24 text-lg">Complete</button>
+            <button className="border-2 w-24 text-lg">Delete</button>
+          </div>
         </div>
       </div>
     </div>
