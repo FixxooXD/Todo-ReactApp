5f530dec9afc6c6790ea2b386462d28e9e1d8966
Fixxoo <rajpalmal40@gmail.com>
2023-03-03 23:32:33 +0530

    Added Responsive ness

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="bg-black sm:w-1/2 p-4">
-        <h1 className="text-white text-center">My Todos</h1>
+    <div className="flex justify-center items-center mt-12">
+      <div className="bg-black w-11/12 sm:w-1/2 p-4">
+        <h1 className="text-white text-center text-2xl">My Todos</h1>
 
         {/* Input  */}
-        <div className="input flex p-2 items-center">
-          <div className="Name flex flex-col ">
-            <label className="text-white">Name</label>
-            <input className="rounded-lg" type={Text} />
+        <div className="input flex flex-col p-2 ">
+          <div className="flex flex-col sm:flex-row">
+            <div className="Name flex flex-col">
+              <label className="text-white">Name</label>
+              <input className="rounded-lg h-8 px-1" type={Text} />
+            </div>
+            <div className="Desc flex flex-col sm:ml-4">
+              <label className="text-white">Description</label>
+              <input className="rounded-lg h-8 px-1" type={Text} />
+            </div>
           </div>
-          <div className="Desc flex flex-col ml-2">
-            <label className="text-white">Description</label>
-            <input className="rounded-lg" type={Text} />
-          </div>
-          <button className="bg-orange-500 text-white ml-auto px-2 rounded-full">
+          <button className="bg-orange-500 text-white h-8 w-28 ml-auto px-2 rounded-full text-xl mt-4 sm:mt-0">
             Add Task
           </button>
         </div>
@@ -31,19 +33,28 @@ function App() {
             <p>Description</p>
           </div>
           <div className="flex justify-between">
-            <button className="border-2 text-lg text-white">Complete</button>
-            <button className="border-2 text-lg text-white">Delete</button>
+            <button className="border-2 text-lg text-white px-1">
+              Complete
+            </button>
+            <button className="border-2 text-lg text-white px-1 ml-2">
+              Delete
+            </button>
           </div>
         </div>
+
+        {/* 2nd Task */}
         <div className="flex border-2 justify-between items-center mt-5 px-2">
           <div>
-            {/* 2nd Task */}
             <p className="text-3xl text-white">Task 2</p>
             <p>Description</p>
           </div>
-          <div className="flex justify-between ">
-            <button className="border-2 text-lg text-white">Complete</button>
-            <button className="border-2 text-lg text-white">Delete</button>
+          <div className="flex justify-between">
+            <button className="border-2 text-lg text-white px-1">
+              Complete
+            </button>
+            <button className="border-2 text-lg text-white px-1 ml-2">
+              Delete
+            </button>
           </div>
         </div>
       </div>
